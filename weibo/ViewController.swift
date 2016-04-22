4a6586b742bce50aa44386ec92316fa4fb23aab9
ping <289966969@qq.com>
2016-04-22 09:39:38 +0800

    Initial Commit

---
+++ weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by SAE on 16/4/22.
+//  Copyright © 2016年 SAE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
