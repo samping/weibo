3efb9c479f474312106e0923b4ae81c8d83a1d18
ping <289966969@qq.com>
2016-04-22 13:50:46 +0800

    first commit

---
+++ weibo/weibo-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  weibo-Bridging-Header.h
+//  weibo
+//
+//  Created by SAE on 16/4/22.
+//  Copyright © 2016年 SAE. All rights reserved.
+//
+
+#ifndef weibo_Bridging_Header_h
+#define weibo_Bridging_Header_h
+
+
+#endif /* weibo_Bridging_Header_h */
